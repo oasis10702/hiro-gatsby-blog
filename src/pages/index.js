e053a54216b2655e7c2f3f070f5eb2017aefb1e1
oasis10702 <oasis10702@gmail.com>
2020-02-09 23:55:43 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
 import Button from "../components/button"
+import { rhythm } from "../utils/typography"
 
 const Home = props => {
   const { data, location } = props
